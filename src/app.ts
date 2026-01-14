64c0225900d35c3ff82734be2c03dd46dc9018f2
mustak <mustakahmmed88@gmail.com>
2026-01-14 10:40:50 +0600

    email verification added

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express from "express"
 import { postRouter } from "./modules/post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-// import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 
-// app.use(cors({
-//     origin:process.env.APP_URL || "http://localhost:4000",
-//     Credential:true
-// }))
+app.use(cors({
+    origin:process.env.APP_URL || "http://localost:4000",
+    credentials:true
+}))
 
 app.use(express.json());
 app.all('/api/auth/*splat', toNodeHandler(auth));
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASSWORD,
+  },
+});
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -26,6 +37,25 @@ export const auth = betterAuth({
         }
     },
     emailAndPassword: { 
-    enabled: true, 
-  }
+    enabled: true,
+    autoSignIn:false,
+    requireEmailVerification:true
+  },
+   emailVerification: {
+    sendVerificationEmail: async ( { user, url, token }, request) => {
+      console.log("****** email verified");
+        const info = await transporter.sendMail({
+        from: '"Maddison Foo Koch" <prismablog@ph.email>',
+        to: "mustakahmmed88@gmail.com",
+        subject: "Hello ✔",
+        text: "Hello world?", // Plain-text version of the message
+        html: "<b>Hello world?</b>", // HTML version of the message
+  });
+   console.log("Message sent:", info.messageId);
+
+
+
+      
+    },
+  },
 });
